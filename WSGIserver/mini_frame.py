e37ca6fe6260cdc124f57b403b5f8df45787720b
latecomer <858348018@qq.com>
2020-07-22 22:54:03 +0800

    没有做什么修改

--- WSGIserver/mini_frame.py
+++ WSGIserver/mini_frame.py
@@ -1,10 +1,14 @@
+def index():
+    return "hjahdfhah"
+
+def login():
+    return "jahhahahahhahhhh"
 
 def application(env,start_response_header):
     start_response_header("200 Ok",[("Content-Type: text/html","charset=utf-8"),("Connection", "keep-alive")])
 
     file_name = env["PATH_INFO"]
     if file_name == "/index.py":
-        return "hahahahahahhf"
+        index()
     elif file_name == "/login.py":
-        return "adsfjdsfjsdklfj"
-
+        login()
