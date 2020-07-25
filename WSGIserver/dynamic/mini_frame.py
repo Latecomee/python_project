3c145f283995d4229570f428570fb1f869aa8189
latecomer <858348018@qq.com>
2020-07-25 16:13:02 +0800

    用字典的方式替换if判断的简单方法

--- WSGIserver/dynamic/mini_frame.py
+++ WSGIserver/dynamic/mini_frame.py
@@ -1,3 +1,7 @@
+import re
+
+URL_FUNC_DICT =dict()
+
 def index():
     with open("./templates/index.html") as f:
         context = f.read()
@@ -9,11 +13,21 @@ def center():
     return context
 
 
+URL_FUNC_DICT = {
+    "/index.py" : index,
+    "/center.py" : center
+    }
+
+
 def application(env,start_response_header):
     start_response_header("200 Ok",[("Content-Type","text/html;charset=utf-8"),("Connection", "keep-alive")])
 
     file_name = env["PATH_INFO"]
-    if file_name == "/index.py":        
-        return index()
-    elif file_name == "/center.py":
-        return center()
+
+    # if file_name == "/index.py":        
+    #     return index()
+    # elif file_name == "/center.py":
+    #     return center()
+
+    func = URL_FUNC_DICT[file_name]
+    return func()
